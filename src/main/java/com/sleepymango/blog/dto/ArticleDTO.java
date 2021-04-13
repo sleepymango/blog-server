433ffc55b59f97c061f729ef332e1fb8e245a1cf
sleepymango <iswenchang@outlook.com>
2021-04-13 11:08:32 +0800

    文章、标签、分类接口

---
+++ src/main/java/com/sleepymango/blog/dto/ArticleDTO.java
@@ -0,0 +1,44 @@
+package com.sleepymango.blog.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sleepymango.blog.entity.Label;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Description ArticleDTO
+ * @Author sleepymango
+ * @Date 2021/4/4 1:44
+ **/
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ArticleDTO implements Serializable {
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String banner;
+
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm")
+    private Date publish;
+
+    private Integer status;
+
+    private List<Label> labels;
+
+    private String author;
+
+    private Long categoryId;
+
+    private String category;
+
+}
